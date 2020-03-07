50322f9f6f6c3749aa7ea43fb85bc5b55212e51c
lamarch <42736089+p0lohx@users.noreply.github.com>
2020-03-07 20:51:20 +0100

    add tabcontent & some transition

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-//Nothing for the moment
+/*
+ *Method to change tab 
+ */
+function openPage(pageName, button){
+    let tabcontents = document.getElementsByClassName('tabcontent');
+
+    for (let i = 0; i < tabcontents.length; i++) {
+        //hide all tabs
+        if(tabcontents[i].id != pageName)
+            tabcontents[i].style.display = 'none';
+        else
+            //show the named tab
+            tabcontents[i].style.display = 'block';
+    }
+}
+
+/*
+ *Set the default tab 
+ */
+document.getElementById('defaultOpen').click();
